07de8026330eef8452aa84308ef057a7de6614c8
Siradafilm_55011212027 <istudents@iClassrooms-iMac-30.local>
2015-01-16 16:05:02 +0700

    Array55011212027Film

--- Array55011212027Film.playground/section-1.swift
+++ Array55011212027Film.playground/section-1.swift
@@ -3,13 +3,44 @@
 import UIKit
 
 var shoppingListExplicit = [String]()
-shoppingListExplicit = ["Eggs", "Milk"]
+shoppingListExplicit = ["Eggs","Milk"]
+//var shoppingListExplicit: [String] = ["Eggs","Milk"]
 
-var shoppingList = ["Eggs", "Milk"]
-println("The shopping list contains \(shoppingList.count) items.")
+var shoppingList = ["Eggs","Milk"]
+println("The shopping list contains\(shoppingList.count) items.")
+//นับ
 shoppingList.append("Flour")
-var shoppingList +="Baking Power"
-var shoppingList +=["Chocolate Spread" , "Cheese" , "Better"]
-var firstItem = shoppingList[0]
+shoppingList += ["Baking Power"]
+shoppingList += ["Chocolate Spreed","Cheese","Butter"]
+//เพิ่มข้อมมูล
+var firstItem = shoppingList[1]
 shoppingList[0] = "Six eggs"
+//เปลี่ยนข้อมูล
+
+// เปลี่ยน var เป็น let จะไม่สามารถเปลี่ยนแปลงค่าได้ จะเกิดการ Eroor
+
+//นับจำนวณข้อมูลในarray
+let possibleTipsInferred = [0.5, 0.18, 0.20]
+let possibleTipExlicit:[Double] = [0.5, 0.18, 0.20]
+var numberOhItem = possibleTipsInferred.count
+
+possibleTipsInferred.isEmpty
+//ถ้าไม่มีข้อมูลเป็น false
+
+//เพิ่มค่าใน array
+shoppingList.insert("pizza", atIndex: 0)
 shoppingList
+
+shoppingList.append("Flour")
+shoppingList
+
+shoppingList[1...3] = ["Banana","Apple"]
+shoppingList
+
+//ลบข้อมูล
+let Flour = shoppingList.removeLast()
+let Apple = shoppingList.removeAtIndex(2)
+shoppingList
+
+shoppingList.removeLast()
+
