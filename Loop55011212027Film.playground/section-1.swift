afc006fa08da1917a6a170f90d52dd297427a43f
Siradafilm_55011212027 <istudents@iClassrooms-iMac-30.local>
2015-01-16 16:50:18 +0700

    Loop

---
+++ Loop55011212027Film.playground/section-1.swift
@@ -0,0 +1,13 @@
+
+
+var array1 = ["abc", "bcd", "cdf"]
+for (var i=0 ; i < array1.count ; i++) {
+    println(array1[i])
+}
+
+var array2 = ["a" , "b" , "c"]
+var str = ""
+for str in array2{
+    println(str)
+}
+
