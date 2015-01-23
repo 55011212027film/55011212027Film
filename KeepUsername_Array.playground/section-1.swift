edb725aebbbacda173811a7d4550f12441a79d47
Siradafilm_55011212027 <istudents@iClassrooms-iMac-30.local>
2015-01-23 16:46:25 +0700

    KeepUser

---
+++ KeepUsername_Array.playground/section-1.swift
@@ -0,0 +1,13 @@
+  class KeepUserName {
+    var UserPass: [String: String] = ["Film": "1234", "Poom": "098"]
+    
+    
+    
+    if UserPass.isEmpty {
+        println("The UserPass dictory is empty.")
+    }else{
+        println("The UserPass dictory is not empty.")
+    }
+  
+  }
+  
---
+++ KeepUsername_Dic.playground/section-1.swift
@@ -0,0 +1,36 @@
+class KeepUsername{
+    var Userpass : [String:String] = ["Film" : "0624", "Poom" : "098", "Chatjan" : "042"]
+    let User:String
+    
+    init(KeepUser:String){
+        self.User = KeepUser
+    }
+    func keepUserpass() ->String {
+        var Keeper:String = ""
+        for (key,value) in Userpass{
+            if(key == User){
+                Keeper=value
+            }
+        }
+        return Keeper
+    }
+    func Add(userAdd:String, passAdd:String){
+        Userpass[userAdd] = passAdd
+    
+    }
+    func Del(userdel:String){
+        Userpass[userdel] = nil
+        
+    }
+    func Edit(useredit:String, passedit:String){
+        Userpass[useredit]=passedit
+    }
+    
+    
+}
+let keeppass = KeepUsername(KeepUser : "Film")
+keeppass.keepUserpass()
+keeppass.Add("paul", passAdd: "1234")
+keeppass.Del("Film")
+keeppass.Edit("poom", passedit: "FabFabFab")
+
